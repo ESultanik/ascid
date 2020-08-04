6bfcba588c091b973187b61e3095f3bbfe481404
Evan Sultanik <evan@sultanik.com>
2020-08-04 12:43:43 -0400

    Allow scripting by only printing offsets to STDOUT

--- ascid/__main__.py
+++ ascid/__main__.py
@@ -10,14 +10,38 @@ def main(argv=None):
 
     parser = argparse.ArgumentParser(description='ascid is the self-referential cycle identifier')
 
-    parser.add_argument('FILE', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument(
+        'FILE',
+        nargs='?',
+        type=argparse.FileType('r'),
+        default=sys.stdin,
+        help='the file to analyze (or STDIN if omitted)'
+    )
 
     args = parser.parse_args(argv[1:])
+
+    all_tty = sys.stderr.isatty() == sys.stdout.isatty()
 
     try:
         s, offsets = find_repeating_strings(args.FILE.read())
 
-        print("Found %i occurrences of the following string at offsets %s:" % (len(offsets), offsets))
-        print(repr(s))
+        if all_tty:
+            sys.stderr.write("Found %i occurrences of the following string at offsets %s:\n" % (len(offsets), offsets))
+            sys.stderr.flush()
+        # Write the actuall offsets to STDOUT, but then clear them using STDERR to hide the output
+        for offset in offsets:
+            sys.stdout.write("%s" % offset)
+            if all_tty:
+                sys.stdout.flush()
+                sys.stderr.write("\r%s\r" % (' ' * len(str(offset))))
+                sys.stderr.flush()
+            sys.stdout.write('\n')
+            if all_tty:
+                sys.stdout.flush()
+                # Move the cursor up a line
+                sys.stderr.write('\033[1A')
+                sys.stderr.flush()
+        if all_tty:
+            sys.stderr.write("%s\n" % repr(s))
     except KeyboardInterrupt:
         pass
