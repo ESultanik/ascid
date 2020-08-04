ff6c56b539003057f3670914b57bd16fce310ab2
Evan Sultanik <evan@sultanik.com>
2020-08-04 10:37:02 -0400

    Adds modern Python packaging

---
+++ ascid/__main__.py
@@ -0,0 +1,20 @@
+import argparse
+import sys
+
+from .ascid import find_repeating_strings
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    parser = argparse.ArgumentParser(description='ascid is the self-referential cycle identifier')
+
+    parser.add_argument('FILE', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+
+    args = parser.parse_args(argv[1:])
+
+    s, offsets = find_repeating_strings(args.FILE.read())
+
+    print("Found %i occurrences of the following string at offsets %s:" % (len(offsets), offsets))
+    print(repr(s))
--- ascid/pyrstrmax.py
+++ ascid/pyrstrmax.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-
-# Except as otherwise commented in this code, ascid is licensed under the GNU GPL v2
-# and is Copyright 2014, Evan A. Sultanik, Ph.D.
-# See the accompanying LICENSE file for the full text of the license.
-
-import sys
-
 # The following code, up until the END_MIT comment, is being redistributed from
 # the py-rstr-max project: https://code.google.com/p/py-rstr-max/
 # and is licensed under the MIT License:
@@ -27,49 +19,53 @@ import sys
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+#
+# It was modified by Evan Sultanik to be forward compatible with Python 3
 
 from array import array
+
+import sys
 
 if sys.version_info.major >= 3:
     xrange = range
 
 
-def direct_kark_sort(s) :
+def direct_kark_sort(s):
     alphabet = [None] + sorted(set(s))
     k = len(alphabet)
     n = len(s)
-    t = dict((c, i) for i,c in enumerate(alphabet))
-    SA = array('i', [0]*(n+3))
-    kark_sort(array('i', [t[c] for c in s]+[0]*3), SA, n, k)
+    t = dict((c, i) for i, c in enumerate(alphabet))
+    SA = array('i', [0] * (n + 3))
+    kark_sort(array('i', [t[c] for c in s] + [0] * 3), SA, n, k)
     return SA[:n]
 
 
-def radixpass(a, b, r, s, n, k) :
-    c = array("i", [0]*(k+1))
-    for i in xrange(n) :
-        c[r[a[i]+s]]+=1
-        
+def radixpass(a, b, r, s, n, k):
+    c = array("i", [0] * (k + 1))
+    for i in xrange(n):
+        c[r[a[i] + s]] += 1
+
     somme = 0
-    for i in xrange(k+1):
+    for i in xrange(k + 1):
         freq, c[i] = c[i], somme
         somme += freq
 
-    for i in xrange(n) :
-        b[c[r[a[i]+s]]] = a[i]
-        c[r[a[i]+s]] += 1
+    for i in xrange(n):
+        b[c[r[a[i] + s]]] = a[i]
+        c[r[a[i] + s]] += 1
 
 
 def kark_sort(s, SA, n, K):
-    n0  = (n+2) // 3
-    n1  = (n+1) // 3
-    n2  = n // 3
+    n0 = (n + 2) // 3
+    n1 = (n + 1) // 3
+    n2 = n // 3
     n02 = n0 + n2
-      
-    SA12 = array('i', [0]*(n02+3))
-    SA0  = array('i', [0]*n0)
 
-    s12 = [i for i in xrange(n+(n0-n1)) if i%3] 
-    s12.extend([0]*3)
+    SA12 = array('i', [0] * (n02 + 3))
+    SA0 = array('i', [0] * n0)
+
+    s12 = [i for i in xrange(n + (n0 - n1)) if i % 3]
+    s12.extend([0] * 3)
     s12 = array('i', s12)
 
     radixpass(s12, SA12, s, 2, n02, K)
@@ -78,85 +74,85 @@ def kark_sort(s, SA, n, K):
 
     name = 0
     c0, c1, c2 = -1, -1, -1
-    for i in xrange(n02) :
-        if s[SA12[i]] != c0 or s[SA12[i]+1] != c1 or s[SA12[i]+2] != c2 :
+    for i in xrange(n02):
+        if s[SA12[i]] != c0 or s[SA12[i] + 1] != c1 or s[SA12[i] + 2] != c2:
             name += 1
             c0 = s[SA12[i]]
-            c1 = s[SA12[i]+1]
-            c2 = s[SA12[i]+2]
-        if SA12[i] % 3 == 1 :
-            s12[SA12[i]//3] = name
-        else :
-            s12[SA12[i]//3 + n0] = name
+            c1 = s[SA12[i] + 1]
+            c2 = s[SA12[i] + 2]
+        if SA12[i] % 3 == 1:
+            s12[SA12[i] // 3] = name
+        else:
+            s12[SA12[i] // 3 + n0] = name
 
-    if name < n02 :
-        kark_sort(s12, SA12, n02, name+1)
-        for i in xrange(n02) :
-            s12[SA12[i]] = i+1
-    else :
-        for i in xrange(n02) :
-            SA12[s12[i]-1] = i
+    if name < n02:
+        kark_sort(s12, SA12, n02, name + 1)
+        for i in xrange(n02):
+            s12[SA12[i]] = i + 1
+    else:
+        for i in xrange(n02):
+            SA12[s12[i] - 1] = i
 
-    s0 = array('i',[SA12[i]*3 for i in xrange(n02) if SA12[i]<n0])
+    s0 = array('i', [SA12[i] * 3 for i in xrange(n02) if SA12[i] < n0])
     radixpass(s0, SA0, s, 0, n0, K)
-  
+
     p = j = k = 0
     t = n0 - n1
-    while k < n :
-        i = SA12[t]*3+1 if SA12[t]<n0 else (SA12[t] - n0)*3 + 2
+    while k < n:
+        i = SA12[t] * 3 + 1 if SA12[t] < n0 else (SA12[t] - n0) * 3 + 2
         j = SA0[p] if p < n0 else 0
 
-        if SA12[t] < n0 :
-            test = (s12[SA12[t]+n0] <= s12[j//3]) if(s[i]==s[j]) else (s[i] < s[j])
-        elif(s[i]==s[j]) :
-            test = s12[SA12[t]-n0+1] <= s12[j//3 + n0] if(s[i+1]==s[j+1]) else s[i+1] < s[j+1]
-        else :
+        if SA12[t] < n0:
+            test = (s12[SA12[t] + n0] <= s12[j // 3]) if (s[i] == s[j]) else (s[i] < s[j])
+        elif (s[i] == s[j]):
+            test = s12[SA12[t] - n0 + 1] <= s12[j // 3 + n0] if (s[i + 1] == s[j + 1]) else s[i + 1] < s[j + 1]
+        else:
             test = s[i] < s[j]
 
-        if(test) :
+        if (test):
             SA[k] = i
             t += 1
-            if t == n02 :
+            if t == n02:
                 k += 1
-                while p < n0 :
+                while p < n0:
                     SA[k] = SA0[p]
                     p += 1
                     k += 1
-        
-        else :
+
+        else:
             SA[k] = j
             p += 1
-            if p == n0 :
+            if p == n0:
                 k += 1
-                while t < n02 :
+                while t < n02:
                     SA[k] = (SA12[t] * 3) + 1 if SA12[t] < n0 else ((SA12[t] - n0) * 3) + 2
                     t += 1
                     k += 1
         k += 1
 
 
-class Rstr_max :
-    def __init__(self) :
+class Rstr_max:
+    def __init__(self):
         self.array_str = []
 
-    def add_str(self, str_unicode) :
+    def add_str(self, str_unicode):
         self.array_str.append(str_unicode)
-    
-    def step1_sort_suffix(self) :
+
+    def step1_sort_suffix(self):
         char_frontier = chr(2)
 
         self.global_suffix = char_frontier.join(self.array_str)
-    
+
         nbChars = len(self.global_suffix)
-        init = [-1]*nbChars
+        init = [-1] * nbChars
         self.idxString = array('i', init)
         self.idxPos = array('i', init)
         self.endAt = array('i', init)
-    
+
         k = idx = 0
-        for mot in self.array_str :
+        for mot in self.array_str:
             last = k + len(mot)
-            for p in xrange(len(mot)) :
+            for p in xrange(len(mot)):
                 self.idxString[k] = idx
                 self.idxPos[k] = p
                 self.endAt[k] = last
@@ -166,68 +162,69 @@ class Rstr_max :
 
         self.res = direct_kark_sort(self.global_suffix)
 
-    def step2_lcp(self) :
+    def step2_lcp(self):
         n = len(self.res)
-        init = [0]*n
+        init = [0] * n
         rank = array('i', init)
         LCP = array('i', init)
-    
+
         s = self.global_suffix
         suffix_array = self.res
         endAt = self.endAt
 
-        for i in xrange(len(self.array_str),n):
+        for i in xrange(len(self.array_str), n):
             v = self.res[i]
             rank[v] = i
 
         l = 0
         for j in xrange(n):
-            if(l > 0) :
+            if (l > 0):
                 l -= 1
             i = rank[j]
-            j2 = suffix_array[i-1]
+            j2 = suffix_array[i - 1]
             if i:
-                while l + j < endAt[j] and l + j2 < endAt[j2] and s[j+l] == s[j2+l]:
+                while l + j < endAt[j] and l + j2 < endAt[j2] and s[j + l] == s[j2 + l]:
                     l += 1
-                LCP[i-1] = l 
+                LCP[i - 1] = l
             else:
                 l = 0
         self.lcp = LCP
 
-    def step3_rstr(self) :
+    def step3_rstr(self):
         prev_len = 0
         idx = 0
         results = {}
-        len_lcp = len(self.lcp) -1
+        len_lcp = len(self.lcp) - 1
 
         class Stack:
             pass
+
         stack = Stack()
         stack._top = 0
         stack.lst_max = []
 
-        if len(self.res) == 0 :
+        if len(self.res) == 0:
             return {}
 
         pos1 = self.res[0]
         for idx in xrange(len_lcp):
             current_len = self.lcp[idx]
-            pos2 = self.res[idx+1]
-            end_ = max(pos1, pos2) + current_len# max(pos1, pos2) + current_len 
+            pos2 = self.res[idx + 1]
+            end_ = max(pos1, pos2) + current_len  # max(pos1, pos2) + current_len
             n = prev_len - current_len
-            if n < 0 :
+            if n < 0:
                 stack.lst_max.append([-n, idx, end_])
                 stack._top += -n
             elif n > 0:
                 self.removeMany(stack, results, n, idx)
-            elif stack._top > 0 and end_ > stack.lst_max[-1][-1] :
+            elif stack._top > 0 and end_ > stack.lst_max[-1][-1]:
                 stack.lst_max[-1][-1] = end_
 
             prev_len = current_len
             pos1 = pos2
-      
-        if(stack._top > 0) :
-            self.removeMany(stack, results, stack._top, idx+1)
+
+        if (stack._top > 0):
+            self.removeMany(stack, results, stack._top, idx + 1)
 
         return results
 
@@ -236,47 +233,20 @@ class Rstr_max :
         while m > 0:
             n, idxStart, maxEnd = stack.lst_max.pop()
             if prevStart != idxStart:
-                id_ = (maxEnd, idxEnd-idxStart+1)
+                id_ = (maxEnd, idxEnd - idxStart + 1)
                 if id_ not in results or results[id_][0] < stack._top:
-                    results[id_] = (stack._top,idxStart)
+                    results[id_] = (stack._top, idxStart)
                 prevStart = idxStart
             m -= n
             stack._top -= n
         if m < 0:
-            stack.lst_max.append([-m, idxStart, maxEnd-n-m])
-            stack._top -= m    
-    
-    def go(self) :
+            stack.lst_max.append([-m, idxStart, maxEnd - n - m])
+            stack._top -= m
+
+    def go(self):
         self.step1_sort_suffix()
         self.step2_lcp()
         r = self.step3_rstr()
         return r
 
 # END_MIT
-
-
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description='ascid is the self-referential cycle identifier')
-    
-    parser.add_argument('FILE', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-
-    args = parser.parse_args(sys.argv[1:])
-
-    rstr = Rstr_max()
-    rstr.add_str(args.FILE.read())
-    r = rstr.go()
-    longest = -1
-    s = None
-    offsets = None
-    if sys.version_info.major < 3:
-        items = r.iteritems()
-    else:
-        items = r.items()
-    for (offset_end, nb), (l, start_plage) in items:
-        if l > longest:
-            longest = l
-            s = rstr.global_suffix[offset_end-l:offset_end].encode('utf-8')
-            offsets = [rstr.idxPos[rstr.res[o]] for o in range(start_plage, start_plage + nb)]
-    print("Found %i occurrences of the following string at offsets %s:" % (len(offsets), offsets))
-    print(repr(s))
